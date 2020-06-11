c38e1212db531e8c41268beff7e28c9563ace940
ExploreNcrack <zhaozhen@ualberta.ca>
2020-06-11 11:51:56 -0600

    c# 8.0 notes

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using Grpc.Net.Client;
 using System;
 using System.Threading.Tasks;
@@ -14,8 +15,15 @@ namespace WeatherConsole
 
             var client = new WeatherClient(channel);
 
-            var forecast = await client.GetWeatherAsync(new Empty());
 
+            var response = client.GetWeatherStream(new Empty());
+            var forecasts = response.ResponseStream.ReadAllAsync();  // support in C# 8.0
+            await foreach(var forecast2 in forecasts) // use await foreach when you have IAsyncEnumerable
+            {
+                Console.WriteLine($"{forecast2.Temperature}");
+            }
+
+            var forecast = await client.GetWeatherAsync(new Empty());
             Console.WriteLine($"{forecast.Temperature}");
 
             Console.WriteLine("none");
